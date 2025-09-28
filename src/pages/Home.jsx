e5581dbc9e6a763d488b0ffb4e3b6730b9256f5c
NaijunWang <wnj123e@gmail.com>
2025-09-28 18:09:37 -0400

    feat: apply webinput component & create and display registered phone list

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,21 @@
 import "../index.css";
 import { NavBar } from "../components/navbar";
 import { Footer } from "../components/footer";
+import { WebInput } from "../components/webInput";
+import { useState } from "react";
 export function Home() {
+  const [phoneList, setPhoneList] = useState([]);
+
+  function addPhone(newPhone) {
+    setPhoneList([...phoneList, newPhone]);
+  }
+
+  const listItems = [];
+
+  for (let i = 0; i < phoneList.length; i++) {
+    listItems.push(<li key={i}>{phoneList[i]}</li>);
+  }
+
   return (
     <div>
       <NavBar />
@@ -13,10 +27,15 @@ export function Home() {
           unique features allows usersto filter books based on genres and complexity,
           among other things. Stay up-to-date through SMS, Signal, and whatsapp notifications
           whenever a book you may be interested in is discussed!
-          </p>
-          <h2>Sign up</h2>
-          <p>Sign up for notifications</p>
-          <h2>Numbers that have signed up</h2>
+        </p>
+        <h2>Sign up</h2>
+        <p>Sign up for notifications</p>
+        <WebInput addPhone={addPhone} />
+        <p className="validation">Please enter a valid 10-digit phone number.</p>
+        <h2>Numbers that have signed up</h2>
+          <ul>
+            {listItems}
+          </ul>
       </div>
       <div className="cta">
         <h2>Want to try it out?</h2>
