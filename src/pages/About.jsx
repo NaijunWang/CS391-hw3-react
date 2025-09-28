a421aadcf45d44e7dd07167a994fb780278afb28
NaijunWang <wnj123e@gmail.com>
2025-09-28 19:35:59 -0400

    feat: complete layout of About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,19 @@ export function About() {
   return (
     <div>
       <NavBar />
+      <div className="content">
+        <h1>About Spark! BookPals</h1>
+        <p>Spark! BookPals was conceived with a simple idea in mind - to make it easy for Boston University community members to find new books to read. We recognizedthe potential of connectingthose who have reada lot with those are just dipping their feet into reading.</p>
+        <h1>About the Author</h1>
+        <div className="author-info">
+          <img className="author-img" src = "/author-image.jpg" />
+          <ul>
+            <li><strong>Bio:</strong> Naijun Wang is a junior majoring in Computer Science at Boston University. He has a passion for exploring new technologies and building innovative projects!</li>
+            <li><strong>Interests:</strong> I enjoy playing vedio games with my friends, jogging, and cooking!</li>
+            <li><strong>Goals:</strong> I hope to share the projects I build with a wider community, so more people can freely use and learn from them.</li>
+          </ul>
+        </div>
+      </div>
       <Footer />
     </div>
   );
